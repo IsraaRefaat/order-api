c3b3ff6497194c4a53d82f8288344adaca40fcd2
Esraa <erefaat591@gmail.com>
2025-07-30 16:23:30 +0300

    RabbitMQConfig fixes

--- src/main/java/com/order/orderapi/rabbit/RabbitMQConfig.java
+++ src/main/java/com/order/orderapi/rabbit/RabbitMQConfig.java
@@ -5,37 +5,28 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
-
-    @Value("${spring.rabbitmq.exchange}")
-    private String exchange;
-
-    @Value("${spring.rabbitmq.response.queue}")
-    private String responseQueue;
-
-    @Value("${spring.rabbitmq.response.routingkey}")
-    private String responseRoutingKey;
+    public static final String QUEUE = "order-notification-queue";
+    public static final String EXCHANGE = "order-notification-exchange";
+    public static final String ROUTING_KEY = "order.notification.key";
 
     @Bean
-    public TopicExchange stockExchange() {
-        return new TopicExchange(exchange);
+    public Queue queue() {
+        return new Queue(QUEUE, true);
     }
 
     @Bean
-    public Queue responseQueue() {
-        return new Queue(responseQueue, true);
+    public TopicExchange exchange() {
+        return new TopicExchange(EXCHANGE);
     }
 
     @Bean
-    public Binding responseBinding(Queue responseQueue, TopicExchange stockExchange) {
-        return BindingBuilder.bind(responseQueue)
-                .to(stockExchange)
-                .with(responseRoutingKey);
+    public Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
     }
 
     @Bean
@@ -45,9 +36,8 @@ public class RabbitMQConfig {
 
     @Bean
     public AmqpTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
-        final RabbitTemplate template = new RabbitTemplate(connectionFactory);
-        template.setMessageConverter(jsonMessageConverter());
-        return template;
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        return rabbitTemplate;
     }
 }
-
